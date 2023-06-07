4468b4b33c37bbced4ffb710f38d4cb6eb43c197
Nakeo <3443472383@qq.com>
2023-06-08 00:55:38 +0800

    fix:线程锁的各种使用方法，互斥锁

--- src/sy/lxsp/a.java
+++ src/sy/lxsp/a.java
@@ -47,8 +47,25 @@ class rl implements Runnable {
         //让多个线程共享num
      private  int num = 100;
      private  boolean loop = true;//退出循环
-    public  synchronized void m(){// 同步方法，在同一个时刻，只能有一个线程来执行m方法
-           if (num <= 0){
+    Object object = new Object();
+    //同步方法(静态的)的锁为当前本身
+    //public synchronized static void m1(){}锁是在rl.class
+
+    public synchronized static void m1(){
+
+    }
+    //如果在静态方法中，实现一个同步代码
+    public static void m2(){
+        synchronized (/*this*/rl.class){//不能直接写this
+            System.out.println("m2");
+        }
+    }
+    //public synchronized void m(){}就是一个同步方法
+    //这时锁在this对象
+    //也可以在代码块上写synchronized，同步代码块
+    public  /*synchronized*/ void m() {// 同步方法，在同一个时刻，只能有一个线程来执行m方法
+        synchronized (/*this*/ object) {//互斥锁还是在this对象 //因为他们操作的是同一个对象object所以也可以实现线程锁
+            if (num <= 0) {
                 System.out.println("售票结束");
                 loop = false;
                 return;
@@ -60,8 +77,9 @@ class rl implements Runnable {
                 throw new RuntimeException(e);
             }
             //打印出线程名，表示这一个窗口售卖了一张票
-            System.out.println("窗口"+Thread.currentThread().getName()+"售出"+"剩余"+(--num));
+            System.out.println("窗口" + Thread.currentThread().getName() + "售出" + "剩余" + (--num));
         }
+    }
           @Override
     public void run() {
         while (loop){
