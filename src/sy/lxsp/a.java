7a5fc171357467701502fa70987f8f39932f1d8d
Nakeo <3443472383@qq.com>
2023-06-07 23:14:27 +0800

    fix:线程锁，并解决sy/lxsp/文件夹下的售票超卖问题

--- src/sy/lxsp/a.java
+++ src/sy/lxsp/a.java
@@ -21,11 +21,13 @@ public class a {
 class  sellt extends Thread {
     //让多个线程共享num
     private static int num = 100 ;
+    private  boolean loop = true;
     @Override
     public void run() {
         while (true){
             if (num <= 0){
                 System.out.println("售票结束");
+                loop = false;
                 break;
             }
             //休眠50毫秒
@@ -40,15 +42,16 @@ class  sellt extends Thread {
     }
 }
 //用实现接口的方式//后面可以值创建一个rl对象就可以不用静态
+//实现接口方式，使用synchronized实现线程同步
 class rl implements Runnable {
         //让多个线程共享num
-    private  int num = 100;
-    @Override
-    public void run() {
-        while (true){
-            if (num <= 0){
+     private  int num = 100;
+     private  boolean loop = true;//退出循环
+    public  synchronized void m(){// 同步方法，在同一个时刻，只能有一个线程来执行m方法
+           if (num <= 0){
                 System.out.println("售票结束");
-                break;
+                loop = false;
+                return;
             }
             //休眠50毫秒
             try {
@@ -59,5 +62,12 @@ class rl implements Runnable {
             //打印出线程名，表示这一个窗口售卖了一张票
             System.out.println("窗口"+Thread.currentThread().getName()+"售出"+"剩余"+(--num));
         }
+          @Override
+    public void run() {
+        while (loop){
+            m();
+
     }
+  }
 }
+
